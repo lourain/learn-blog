8c8118f84cb874172c6230a2374436fc93627f92
liyiwei <liyiwei@jisuhebao.com>
2018-06-20 18:32:26 +0800

    620

--- route/admin.js
+++ route/admin.js
@@ -11,8 +11,6 @@ const db = mysql.createPool({
 })
 
 router.use((req, res, next) => {
-    console.log(req.session.test)
-    // console.log(req.session['admin_id'])
     if (!req.session['admin_id'] && req.url != '/login') {
         res.redirect('/admin/login')
     } else {
@@ -21,13 +19,10 @@ router.use((req, res, next) => {
 })
 
 router.get('/login', (req, res) => {
-    console.log(req.session.test)
-    // console.log(req.session['admin_id'])
     res.render('../template/admin/login.ejs')
 })
 
 router.post('/login', (req, res) => {
-    req.session.test = 'test'
     var username = req.body.username
     var password = common.md5(req.body.password + common.md5_suffix)
     db.query(`SELECT * FROM admin_table WHERE username='${username}'`, (err, data) => {
@@ -49,8 +44,50 @@ router.post('/login', (req, res) => {
     })
 })
 router.get('/',(req,res)=>{
-    console.log(req.session.test);
-    
-    res.send('nice')
+    res.render('../template/admin/index.ejs',{})
 })
+router.get('/banners',(req,res)=>{
+//删除或是修改操作
+    var act = req.query.act
+    var id = req.query.id 
+    if(act == 'amend'){
+        
+    }else if(act == 'delete'){
+        console.log(12313123);
+        db.query(`DELETE FROM banner_table WHERE ID = '${id}'`, (err, banners) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('database error').end()
+            } else {
+               res.redirect('/admin/banners').end()
+            }
+        })
+    }
+    db.query(`SELECT * FROM banner_table`,(err,banners)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('database error').end()
+        }else{
+            res.render('../template/admin/banner.ejs',{banners:banners})
+        }
+    })
+})
+router.post('/banners',(req,res)=>{
+    var title = req.body.title
+    var description = req.body.description
+    var href = req.body.href
+    if(!title || !description || !href){
+        res.status(400).send('参数错误').end()
+    }else{
+        db.query(`INSERT INTO banner_table (title,description,href) value ('${title}','${description}','${href}')`,(err,data)=>{
+            if (err) {
+                console.error(err);
+                res.status(500).send('database error').end()
+            }else{
+                res.redirect('/admin/banners')
+            }
+        })
+    }
+})
+
 module.exports = router
